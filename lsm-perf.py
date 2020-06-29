9fe96a78c65c3a08106d20dd1d4873bd501ca7c1
Paul Renauld <paulrenauld27@gmail.com>
2020-06-29 14:05:27 +0000

    update naming + basedirpath based on comments

--- lsm-perf.py
+++ lsm-perf.py
@@ -5,6 +5,7 @@ import time
 import plumbum
 import sys
 import statistics
+import os
 from plumbum import local, SshMachine  # TODO: requirements
 
 
@@ -23,7 +24,7 @@ def main(args):
             for kernel in args.kernels:
                 results = evaluating_kernel(
                     kernel_path=kernel.name,
-                    img_path=args.image.name,
+                    filesystem_path=args.image.name,
                     workload_path=args.workload.name,
                     keyfile=args.key.name
                 )
@@ -35,15 +36,15 @@ def main(args):
     return 0
 
 
-def evaluating_kernel(kernel_path, img_path, workload_path, keyfile):
+def evaluate_kernel(kernel_path, filesystem_path, workload_path, keyfile):
     """Starts a VM with the kernel and evaluates its
     performances on the provided workload
     """
     results = []
-    name = kernel_path[kernel_path.rfind('/') + 1:]
+    name = os.path.basename(kernel_path)
     print_eta(name, info='connecting')
 
-    with VM(kernel_path, img_path, keyfile) as vm:
+    with VM(kernel_path, filesystem_path, keyfile) as vm:
         vm.scp_to(workload_path, ON_VM_WORKLOAD_PATH)
         work_cmd = vm.ssh[ON_VM_WORKLOAD_PATH]
 
@@ -69,9 +70,9 @@ class VM:
     """Represents a qemu VM with an SSH connection.
     To use with the `with` construct
     """
-    def __init__(self, kernel_path, img_path, keyfile):
+    def __init__(self, kernel_path, filesystem_path, keyfile):
         """Starts the qemu VM"""
-        qemu_args = construct_qemu_args(kernel_path, img_path)
+        qemu_args = construct_qemu_args(kernel_path, filesystem_path)
         self.process = local['qemu-system-x86_64'].popen(qemu_args)
         self.ssh = None
         self.key = keyfile
@@ -107,7 +108,7 @@ class VM:
         plumbum.path.utils.copy(fro, to)
 
 
-def construct_qemu_args(kernel_path, img_path):
+def construct_qemu_args(kernel_path, filesystem_path):
     """Qemu arguments similar to what `vm start` produces"""
     return [
         '-nographic',
@@ -119,7 +120,7 @@ def construct_qemu_args(kernel_path, img_path):
         '-append', 'console=ttyS0,115200 root=/dev/sda rw nokaslr',
         '-smp', '4',
         '-m', '4G',
-        '-drive', 'if=none,id=hd,file=%s,format=raw' % img_path,
+        '-drive', 'if=none,id=hd,file=%s,format=raw' % filesystem_path,
         '-device', 'virtio-scsi-pci,id=scsi',
         '-device', 'scsi-hd,drive=hd',
         '-device', 'virtio-rng-pci,max-bytes=1024,period=1000',
